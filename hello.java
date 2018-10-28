70828971c426efc30093ef3cd2072afbc894f5cb
computer system engineering <cse@localhost.localdomain>
2018-10-28 13:54:45 +0900

    Commit 4

--- hello.java
+++ hello.java
@@ -5,5 +5,6 @@ public class hello
     System.out.println("hello");
     System.out.println("For commit 2");
     System.out.println("For commit 3");
+    System.out.println("For commit 4");
   }
 }
